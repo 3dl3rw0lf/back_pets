32bcd0f47d5fe9a368a69564713096cfea578634
Gustavo Arauz <arauz.gus@gmail.com>
2024-07-12 18:42:56 -0300

    config II

--- config/config.js
+++ config/config.js
@@ -8,14 +8,14 @@ dotenv.config({
 });
 
 const config = {
-  host: process.env.MYSQL_ADDON_HOST,
-  user: process.env.MYSQL_ADDON_USER,
-  pass: process.env.MYSQL_ADDON_PASSWORD,
-  port_db: process.env.MYSQL_ADDON_PORT,
-  port_server: process.env.PORT_SERVER,
-  secretKey: process.env.SECRET_KEY,
-  tokenExpiresIn: process.env.TOKEN_EXPIRES_IN,
-  database: process.env.MYSQL_ADDON_DB
+  host: process.env.MYSQL_ADDON_HOST || process.env.HOST,
+  user: process.env.MYSQL_ADDON_USER || process.env.USER,
+  pass: process.env.MYSQL_ADDON_PASSWORD || process.env.PASS,
+  port_db: process.env.MYSQL_ADDON_PORT || process.env.PORT_DB,
+  port_server: process.env.PORT_SERVER || 3000,
+  secretKey: process.env.SECRET_KEY || 'secret',
+  tokenExpiresIn: process.env.TOKEN_EXPIRES_IN || '1h',
+  database: process.env.MYSQL_ADDON_DB || 'pets_lost'
 };
 
 module.exports = config;
