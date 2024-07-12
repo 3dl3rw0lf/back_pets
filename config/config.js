a59da044ae5b2d293b75ac9569599a63dfd77a55
Gustavo Arauz <arauz.gus@gmail.com>
2024-07-12 18:45:54 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 
 dotenv.config({
   path: path.resolve(__dirname, `${env}.env`)
