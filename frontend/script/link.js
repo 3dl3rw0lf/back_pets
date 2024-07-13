abcc16fd546d67d3dcda8f380b6f2502ba07eb6f
Gustavo Arauz <arauz.gus@gmail.com>
2024-07-12 21:20:09 -0300

    Update link.js
    test render link

--- frontend/script/link.js
+++ frontend/script/link.js
@@ -2,19 +2,23 @@ export const links = {
   pages: [
     {
       page: 'Pichis',
-      link: '/frontend/index.html'
+      // link: '/frontend/index.html'
+      link: '/index.html'
     },
     {
       page: 'Mascotas Perdidas',
-      link: '/frontend/templates/lost_pets.html'
+      // link: '/frontend/templates/lost_pets.html'
+      link: '/templates/lost_pets.html'
     },
     {
       page: 'Mascotas Encontradas',
-      link: '/frontend/templates/pets_found.html'
+      // link: '/frontend/templates/pets_found.html'
+      link: '/templates/pets_found.html'
     },
     {
       page: 'Encontrar a mi mascota',
-      link: '/frontend/templates/contact.html'
+      // link: '/frontend/templates/contact.html'
+      link: '/templates/contact.html'
     }
   ]
 };
